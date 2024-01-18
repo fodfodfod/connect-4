32060e00f8b3d49c134b600cefd8ddf2aa9cd0c1
fodfodfod <94200657+fodfodfod@users.noreply.github.com>
2024-01-18 13:21:23 -0500

    add non working win

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
 use std::result::Result;
+use std::time::SystemTime;
+//number of rows
+const ROWS: i32 = 6;
+//number of columns
+const COLUMNS: i32 = 7;
  
 fn main() {
     println!("Hello, world!");
     let mut board = Board::new();
+    let now = SystemTime::now();
+    let mut column = 0;
+    /*loop{
+        match board.add_piece(column, Spot::Red){
+            Ok(_) => (),
+            Err(_) => column += 1
+        }
+        if column > 7{
+            break;
+        }
+    }
+    println!("{}", now.elapsed().unwrap().as_nanos());
+    */
     loop {
         //player
         println!("you are red");
@@ -10,43 +28,61 @@ fn main() {
         let mut user_input = String::new();
         let _ = std::io::stdin().read_line(&mut user_input);
         println!("{}", user_input.pop().unwrap());
-        let _ = match user_input.parse::<u8>(){
-            Ok(value) => board.add_piece(value, Spot::RED),
+        let position: i32 = match user_input.parse::<i32>(){
+            Ok(value) => match board.add_piece(value, Spot::Red){
+                Ok(position) => position,
+                Err(..) => {
+                    println!("you bad (not a valid number)");
+                    continue
+                }
+            },
             _ => {
-                println!("you bad");
+                println!("you bad! (not a number)");
                 continue;
             }
         };
-        board.add_piece(0, Spot::YELLOW);
+        if board.check_win(Spot::Red, position){
+            println!("red wins");
+            break;
+        }
+        println!(" . ");
+        let position = board.add_piece(0, Spot::Yellow).unwrap();
+        if board.check_win(Spot::Yellow, position){
+            println!("yellow wins");
+        }
         board.display();
     }
     
+    
 }
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq)]
 enum Spot{
-    RED,
-    YELLOW,
-    BLANK
+    Red,
+    Yellow,
+    Blank
 }
 
 
 struct Board{
-    board: [Spot; 64],
+    board: [Spot; (ROWS * COLUMNS) as usize],
 }
 
 impl Board{
     fn new() -> Self {
         Board {
-            board: [Spot::BLANK; 64],
+            board: [Spot::Blank; (ROWS * COLUMNS) as usize],
         }
     }
-    fn add_piece(&mut self, column: u8, color: Spot) -> Result<(), &'static str> {
+    fn add_piece(&mut self, column: i32, color: Spot) -> Result<i32, &'static str> {
+        if(column >= COLUMNS){
+            return Err("column to high");
+        }
         let mut row = 0;
-        while row < 8 {
-            match self.board[(column as usize) + (8 * row) as usize] {
-                Spot::BLANK => {
-                    self.board[(column as usize) + (8 * row) as usize] = color;
-                    return Ok(());
+        while row < ROWS {
+            match self.board[(column as usize) + (COLUMNS * row) as usize ] {
+                Spot::Blank => {
+                    self.board[(column as usize) + (COLUMNS* row) as usize ] = color;
+                    return Ok(column + (COLUMNS* row));
                 }
                 _ => (),
             }
@@ -55,17 +91,91 @@ impl Board{
         Err("Column is full")
     }
     fn display(&self) {
-        for row in (0..8).rev() {
-            for col in 0..8 {
-                match self.board[col + (8 * row) as usize] {
-                    Spot::RED => print!("R "),
-                    Spot::YELLOW => print!("Y "),
-                    Spot::BLANK => print!("_ "),
+        for row in (0..ROWS).rev() {
+            for col in 0..COLUMNS {
+                match self.board[(col + (COLUMNS * row)) as usize] {
+                    Spot::Red => print!("R "),
+                    Spot::Yellow => print!("Y "),
+                    Spot::Blank => print!("_ "),
                 }
             }
             println!();
         }
         println!();
         println!("0 1 2 3 4 5 6 7");
+    }
+    fn check_win(&self, last_team: Spot, last_piece: i32) -> bool{
+        let last_row = (last_piece % COLUMNS) as usize;
+        //println!("last row {}", last_row);
+        let last_col = (last_piece / COLUMNS) as usize;
+        //println!("last column {}", last_col);
+
+        // Check horizontally
+        let mut count = 0;
+        for col in 0..COLUMNS as usize {
+            if self.board[col + last_row * COLUMNS as usize] == last_team {
+                count += 1;
+            } else {
+                count = 0;
+            }
+
+            if count >= 4 {
+                return true;
+            }
+        }
+
+        // Check vertically
+        count = 0;
+        for row in 0..ROWS as usize {
+            if self.board[last_row + row * COLUMNS as usize] == last_team {
+                count += 1;
+            } else {
+                count = 0;
+            }
+
+            if count >= 4 {
+                return true;
+            }
+        }
+
+        // Check diagonally (from top-left to bottom-right)
+        count = 0;
+        let mut row = last_row as i32 - last_col as i32;
+        let mut col = 0;
+        while row < ROWS && col < COLUMNS {
+            if row >= 0 && self.board[(col + row * COLUMNS) as usize] == last_team {
+                count += 1;
+            } else {
+                count = 0;
+            }
+
+            if count >= 4 {
+                return true;
+            }
+
+            row += 1;
+            col += 1;
+        }
+
+        // Check diagonally (from top-right to bottom-left)
+        count = 0;
+        let mut row = last_row as i32 + last_col as i32;
+        let mut col = COLUMNS - 1;
+        while row >= 0 && row < ROWS && col >= 0 {
+            if self.board[(col + row * COLUMNS ) as usize] == last_team {
+                count += 1;
+            } else {
+                count = 0;
+            }
+
+            if count >= 4 {
+                return true;
+            }
+
+            row += 1;
+            col -= 1;
+        }
+
+        false
     }
 }
